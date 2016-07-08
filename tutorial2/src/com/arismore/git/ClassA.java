8b6ae4f0e61bea5ad668cd6d31ce6a634f2f325a
Louay Saadi <Louay Saadi@ARIS635.arismore>
2016-07-08 14:36:44 +0100

    from me update

--- tutorial2/src/com/arismore/git/ClassA.java
+++ tutorial2/src/com/arismore/git/ClassA.java
@@ -5,4 +5,20 @@ public class ClassA {
 	
 	private int y;
 
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
 }
